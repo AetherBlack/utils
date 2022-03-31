f81ec2df5dd695141dbc0016c56cd85c6e4bad63
yohan <yohan.fevre@protonmail.com>
2022-03-31 17:16:30 +0200

    update(search_asm) : Add registers table/argument

--- Search_asm/search_asm.py
+++ Search_asm/search_asm.py
@@ -40,14 +40,51 @@ class AsmInstruction:
             print("".ljust(len(title) + 2, "-"))
             print(title_content.replace(value2replace, replace2value))
             print()
+    
+    @staticmethod
+    def register() -> str:
+        return """+-----+------+------+----+------+
+| 64  | 32   | 16   | 8h | 8l   |
++-----+------+------+----+------+
+| rax | eax  | ax   | ah | al   |
+| rbx | ebx  | bx   | bh | bl   |
+| rcx | ecx  | cx   | ch | cl   |
+| rdx | edx  | dx   | dh | dl   |
+| rsi | esi  | si   |    | sil  |
+| rdi | esi  | di   |    | dil  |
+| rbp | ebp  | bp   |    | bpl  |
+| rsp | esp  | sp   |    | spl  |
+| r8  | r8d  | r8w  |    | r8b  |
+| r9  | r9d  | r9w  |    | r9b  |
+| r10 | r10d | r10w |    | r10b |
+| r11 | r11d | r11w |    | r11b |
+| r12 | r12d | r12w |    | r12b |
+| r13 | r13d | r13w |    | r13b |
+| r14 | r14d | r14w |    | r14b |
+| r15 | r15d | r15w |    | r15b |
++-----+------+------+----+------+"""
+
+    @staticmethod
+    def help() -> None:
+        print(f"{sys.argv[0]} [asm instruction]")
+        print(f"{sys.argv[0]} --registers: Print table of register")
+        print(f"{sys.argv[0]} --help: Print this message")
 
 #Check for asm_instruction in args
 if len(sys.argv) != 2:
-    print(f"{sys.argv[0]} [asm instruction]")
+    AsmInstruction.help()
     exit(1)
 
 #Get the asm_instruction
 asm_instruction = sys.argv[1]
+
+# Check command arguments
+if asm_instruction == "--registers":
+    print(AsmInstruction.register())
+    exit(0)
+elif asm_instruction == "--help":
+    AsmInstruction.help()
+    exit(0)
 
 #Requests
 res = requests.get(URL.format(asm_instruction))
