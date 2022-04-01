851f93a604a5bd1523a36cf2047c970def3efe9d
yohan <yohan.fevre@protonmail.com>
2022-04-01 14:01:38 +0200

    update(search_asm) : Change the way the documentation is get

--- Search_asm/search_asm.py
+++ Search_asm/search_asm.py
@@ -5,44 +5,42 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 
-#URL of man page
+# URL of man page
 URL = "https://www.aldeid.com/wiki/X86-assembly/Instructions/{0}"
+PAGE_TAGS = ["h1", "p", "ul", "dl", "h2", "pre"]
 
 class AsmInstruction:
 
-    def __init__(self, content, text_list):
-        self.content = content
-        self.text_list = text_list
+    def __init__(self):
+        """
+        Assembly Instruction class
+        """
 
-    def get_content(self, title, stop_value, value2replace="", replace2value="", next_title=None):
+    @staticmethod
+    def print_assembly_manual(page_content: BeautifulSoup) -> None:
+        """
+        Print to the stdout the content of the page.
+        """
+        tags = page_content.find_all(PAGE_TAGS)
 
-        #Check examples
-        if title in self.content:
-
-            #Get the examples
-            actual_title = str()
-            title_content = str()
-            index = 0
-
-            #Get index of the content for the given title
-            while actual_title != title:
-                actual_title = self.text_list[index]
-                index += 1
-
-            #Get the content
-            while self.text_list[index] != stop_value and self.text_list[index] != next_title:
-                #Add text
-                title_content += self.text_list[index] + "\n"
-                index += 1
-
-            #Print
-            print(f"{title} :")
-            print("".ljust(len(title) + 2, "-"))
-            print(title_content.replace(value2replace, replace2value))
-            print()
+        # Get the first title
+        for index, tag in enumerate(tags):
+            if "h1" in str(tag):
+                break
+        
+        # Print page content
+        for tag in tags[index:]:
+            value = "".join(tag.strings)
+            if value == "Comments": break
+            print(value)
+            if "h1" in str(tag):
+                print("".ljust(len(value), "-"))
     
     @staticmethod
     def register() -> str:
+        """
+        Return the table of register.
+        """
         return """+-----+------+------+----+------+
 | 64  | 32   | 16   | 8h | 8l   |
 +-----+------+------+----+------+
@@ -66,48 +64,51 @@ class AsmInstruction:
 
     @staticmethod
     def help() -> None:
-        print(f"{sys.argv[0]} [asm instruction]")
-        print(f"{sys.argv[0]} --registers: Print table of register")
-        print(f"{sys.argv[0]} --help: Print this message")
+        """
+        Print arguments for the script.
+        """
+        script_name = sys.argv.pop(0)
+        print(f"{script_name} [asm instruction]")
+        print(f"{script_name} --registers: Print table of register")
+        print(f"{script_name} --help: Print this message")
 
-#Check for asm_instruction in args
-if len(sys.argv) != 2:
-    AsmInstruction.help()
-    exit(1)
 
-#Get the asm_instruction
-asm_instruction = sys.argv[1]
+if __name__ == "__main__":
+    # Check for asm_instruction in args
+    if len(sys.argv) != 2:
+        AsmInstruction.help()
+        exit(1)
 
-# Check command arguments
-if asm_instruction == "--registers":
-    print(AsmInstruction.register())
-    exit(0)
-elif asm_instruction == "--help":
-    AsmInstruction.help()
-    exit(0)
+    # Get the asm_instruction
+    asm_instruction = sys.argv[1]
 
-#Requests
-res = requests.get(URL.format(asm_instruction))
+    # Check command arguments
+    if asm_instruction == "--registers":
+        print(AsmInstruction.register())
+        exit(0)
+    elif asm_instruction == "--help":
+        AsmInstruction.help()
+        exit(0)
 
-#Check the status code
-if res.status_code == 200:
-    #Parse response
-    soup = BeautifulSoup(res.text, "html.parser")
+    # Get the content of the page
+    res = requests.get(
+            URL.format(asm_instruction),
+            headers={"User-Agent": "Mozilla/5.0"}
+        )
 
-    #Get each line
-    text_list = soup.get_text().splitlines()
+    # Check the status code
+    if res.status_code == 200:
+        # Parse response
+        soup = BeautifulSoup(res.text, "html.parser")
 
-    #Get instance
-    asm = AsmInstruction(res.text, text_list)
+        # Get each line
+        parser_content = soup.find("div", {"class": "mw-parser-output"})
 
-    #Print content
-    asm.get_content("Description", "", next_title="Syntax")
-    asm.get_content("Syntax", "", next_title="Examples")
-    asm.get_content("Examples", "Comments")
+        # Print content of the assembly instruction manual
+        AsmInstruction.print_assembly_manual(parser_content)
+        exit(0)
 
-    exit(0)
-
-else:
-    #Error and quit
-    print(f"[*] '{asm_instruction}' Not found ! ")
-    exit(1)
+    else:
+        #Error and quit
+        print(f"[*] '{asm_instruction}' Not found ! ")
+        exit(1)
